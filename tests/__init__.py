411fb15e82c091ede55d47c2bd24c6b159b641e5
Henri-Langenhoven <93364878+Henri-Langenhoven@users.noreply.github.com>
2025-01-20 11:00:57 +0200

    Explicitly specify encoding='utf-8' when opening files in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -16,11 +16,11 @@ with open(join(dirname(abspath(__file__)), "test_generation.py"), "w") as f:
     for file in listdir(join(dirname(abspath(__file__)), "in")):
         file_identifier = re.sub(r"\W+", "", file)
         method = f"""\n    def test_{file_identifier}(self):
-        with open(join(self.in_path, '{file}')) as f:
+        with open(join(self.in_path, '{file}'), encoding='utf-8') as f:
             out = generate_toc(f.read())
-            with open(join(self.out_path, '{file}'), 'w') as f_out:
+            with open(join(self.out_path, '{file}'), 'w', encoding='utf-8') as f_out:
                 f_out.write(out)
-        with open(join(self.ref_path, '{file}')) as f:
+        with open(join(self.ref_path, '{file}'), encoding='utf-8') as f:
             self.assertEqual(f.read(), out)\n"""
         methods += method
     f.write(re.sub(r"(?<=# PLACEHOLDER_BEGIN).*(?=# PLACEHOLDER_END)", methods, content, flags=re.DOTALL))
